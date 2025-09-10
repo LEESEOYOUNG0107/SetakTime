53361957fde741b90831294b40a1ccbabba4d965
limsso128 <s2412@e-mirim.hs.kr>
2025-09-10 15:34:01 +0900

    db

--- db.js
+++ db.js
@@ -11,3 +11,27 @@ require('dotenv').config();
 // })
 // .then(() => console.log('MongoDB에 성공적으로 연결되었습니다.'))
 // .catch(err => console.error('MongoDB 연결 오류:', err));
+
+//mysql2 모듈 불러오기(DB와 연결할때 사용)
+const mysql = require('mysql2');
+
+//Node.js와 연결
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: "1234",
+    database: 'SetakTime'
+});
+
+//db 사용하기
+db.getConnection((err, conn) => {
+    if(err) {
+        console.err("DB 연결 실패", err);
+    } else {
+        console.log("DB 연결 성공");
+        conn.release(); //→ 풀에서 가져온 연결을 사용 후 반환
+    }
+});
+
+// 다른 파일에서 require('./db')로 불러와서 사용 가능
+module.exports = db;
