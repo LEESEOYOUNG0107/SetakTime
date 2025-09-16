464516945627f210f4f762cffd79cddce44de44f
limsso128 <s2412@e-mirim.hs.kr>
2025-09-16 13:36:59 +0900

    메뉴바 틀잡기

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const express = require('express');
-const app = express(); 
+const app = express();
+const path = require('path');
+const authRouter = require('./routes/auth.js'); 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('style')); 
 
-//라우터 파일 가져오기
-const signupRouter = require('./routes/auth/signup');
-const loginRouter = require('./routes/auth/login');
+//라우터 연결
+app.use('/auth', authRouter);
 
 //라우터 연결하기
 app.use('/register', signupRouter);
 app.use('/login', loginRouter);
 
-const PORT = 3000; // 원하는 포트 번호로 변경 가능
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/views/signup.html');
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html');
+app.listen(3000, () => {
+  console.log('서버가 포트 3000에서 실행 중입니다.');
 });
