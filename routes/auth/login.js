4cda6d1bb23a103bc046bb5584f42725aff08695
limsso128 <s2412@e-mirim.hs.kr>
2025-10-14 12:45:25 +0900

    로그인 성공

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const db = require('../../db');
 
 // GET 요청: 로그인 페이지
 router.get('/', (req, res) => {
@@ -10,8 +11,23 @@ router.get('/', (req, res) => {
 // POST 요청: 로그인 처리
 router.post('/', (req, res) => {
     const { userid, password } = req.body;
-    // DB에서 사용자 확인 후 로그인 처리
-    res.send('로그인 시도'); // 나중에 로그인 로직으로 변경
+
+    // DB에서 사용자 확인
+    const sql = 'SELECT * FROM information WHERE userid = ? AND password = ?';
+    db.query(sql, [userid, password], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('DB 오류 발생');
+        }
+
+        if (result.length > 0) {
+            // 로그인 성공
+            res.send('success');
+        } else {
+            res.send('fail');
+        }
+    });
 });
+
 
 module.exports = router;
